bdf3084bef93e544a0b1450ae04f3db083fb3550
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-09-19 21:18:02 +0200

    Import example data in parallel

--- import-examples.sh
+++ import-examples.sh
@@ -4,5 +4,5 @@ for i in $(find gsim-raml-schema/examples/_main -type f)
 do
   ENTITY=$(echo $i | sed 's:gsim-raml-schema/examples/_main/::' | sed 's:\([^_]*\).*:\1:')
   ID=$(jq -r .id "$i")
-  curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i"
+  curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
 done
