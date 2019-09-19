4b0a1189571f8f27bcd98bcfe715a487aecfad4f
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-09-19 21:14:35 +0200

    Use Solr search provider

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module no.ssb.lds.server {
     requires no.ssb.lds.core;
     requires no.ssb.config;
     requires org.slf4j;
+    requires org.apache.commons.logging; // needed to use the solr search provider
     requires no.ssb.rawdata.api;
     requires no.ssb.service.provider.api;
 
