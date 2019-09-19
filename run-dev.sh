2c2414209065c2822780fbc5b124eedf18543755
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-09-19 20:18:28 +0200

    Correct volume prefix for clean command

--- run-dev.sh
+++ run-dev.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "clean" ]; then
   echo "Cleaning existing associated volumes and data"
   docker-compose down
-  docker volume rm $(docker volume ls -q -f "name=ldsserver")
+  docker volume rm $(docker volume ls -q -f "name=linked-data-store-docker_")
 else
   echo "Reusing existing volumes and data"
 fi
