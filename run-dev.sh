0992abf1b4eaf1eb7996ef1b0250f91d57e51510
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-09-19 22:39:23 +0200

    Added update-images option

--- run-dev.sh
+++ run-dev.sh
@@ -8,4 +8,11 @@ else
   echo "Reusing existing volumes and data"
 fi
 
+if [ "$1" == "update-images" ]; then
+  for i in neo4j:3.5 postgres:11-alpine solr adminer; do
+    docker pull $i &
+  done
+  wait
+fi
+
 docker-compose up --remove-orphans
