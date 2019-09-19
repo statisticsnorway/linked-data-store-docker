0e6f046442271e4eb2f422814090b3973b285a40
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-09-19 22:37:16 +0200

    Wait for curl commands to finish before exiting script

--- import-examples.sh
+++ import-examples.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-for i in $(find gsim-raml-schema/examples/_main -type f)
-do
+for i in $(find gsim-raml-schema/examples/_main -type f); do
   ENTITY=$(echo $i | sed 's:gsim-raml-schema/examples/_main/::' | sed 's:\([^_]*\).*:\1:')
   ID=$(jq -r .id "$i")
   curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
 done
+
+wait
