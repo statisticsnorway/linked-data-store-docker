52ada3f74125943170990017f1a9e95502afb145
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-05-05 10:24:30 +0200

    Update postgres and neo4j versions

--- build-dev.sh
+++ build-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in statisticsnorway/lds-browser-dev neo4j:3.5 postgres:11-alpine solr adminer; do
+for i in statisticsnorway/lds-browser-dev neo4j:4 postgres:12-alpine solr adminer; do
   docker pull $i &
 done
 
